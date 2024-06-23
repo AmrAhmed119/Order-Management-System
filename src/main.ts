378db857ca2e756fecf357ff8f9c246f77bef260
Amr Ahmed <es-AmrA.Abdelazim2025@alexu.edu.eg>
2024-06-23 12:42:04 +0300

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('APIs handling orders, products, and Users')
+    .setVersion('1.0')
+    .addTag('E-commerce')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
